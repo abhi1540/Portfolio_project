626001442243058d0ba9605cf6ebaca88935324f
abhisek <abhisek1651990@gmail.com>
2018-05-12 18:52:02 +0530

    added jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.conf import settings
+from .models import Job
 # Create your views here.
+
+
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'MEDIA_URL': settings.MEDIA_URL, 'jobs': jobs})
+
